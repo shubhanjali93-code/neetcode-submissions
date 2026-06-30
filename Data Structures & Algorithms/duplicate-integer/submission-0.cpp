064c4e234e9cbae3cb991372b06a6454aeab8042
Shubhanjali <shubhanjali2521@gmail.com>
2026-06-30 11:28:07 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        set<int>s;
+        for(int x : nums){
+            if(s.count(x)) return true;
+            s.insert(x);
+        }
+        return false;
+    }
+};
